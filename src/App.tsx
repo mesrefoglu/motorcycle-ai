18c3f5c61b4ce6ee4b16fe5bad8be53dd0f90e8c
Mustafa Mesrefoglu <qedized@gmail.com>
2025-03-05 16:01:52 -0500

    prettier on App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
 // App.tsx
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LandingPage from './components/LandingPage';
-import QuizPage from './components/Quiz';
-import BikeResults from './components/BikeResults';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "./components/LandingPage";
+import QuizPage from "./components/Quiz";
+import BikeResults from "./components/BikeResults";
 
 const App: React.FC = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/quiz" element={<QuizPage />} />
-        <Route path="/results" element={<BikeResults />} />
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<LandingPage />} />
+                <Route path="/quiz" element={<QuizPage />} />
+                <Route path="/results" element={<BikeResults />} />
+            </Routes>
+        </Router>
+    );
 };
 
 export default App;
